c5cd5ced776a6ae970b016635f824b85ea5e4704
Can <canavci2016@gmail.com>
2019-10-17 11:31:54 +0300

    Styled product item component

--- constants/Colors.js
+++ constants/Colors.js
@@ -1,4 +1,4 @@
 export default {
     primary: '#C21858',
-    accent: '#FFC1O7'
+    accent: '#ffc107'
 }
--- screens/shop/ProductsOverViewScreen.js
+++ screens/shop/ProductsOverViewScreen.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import {FlatList, Text} from "react-native";
+import {FlatList, Text, StyleSheet} from "react-native";
 import {useSelector} from "react-redux";
-
+import ProductItem from '../../components/shop/ProductItem';
 
 const ProductsOverViewScreen = props => {
     const products = useSelector(state => state.products.availableProducts);
-    return <FlatList data={products} keyExtractor={item => item.id} renderItem={({item}) => <Text>{item.title}</Text>}/>
+    return <FlatList data={products} keyExtractor={item => item.id}
+                     renderItem={({item}) => <ProductItem image={item.image} title={item.title}
+                                                          price={item.price}/>}/>
 };
+
+ProductsOverViewScreen.navigationOptions = {
+    headerTitle: 'All Products',
+}
 
 export default ProductsOverViewScreen;
