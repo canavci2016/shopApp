73f2163c90be4d702c719f4939228dfd7cb000e0
Can <canavci2016@gmail.com>
2019-11-13 11:35:56 +0300

    Implemented update and delete product actions on remote server

--- store/actions/product.js
+++ store/actions/product.js
@@ -1,17 +1,14 @@
 import Product from "../../models/product";
-
 export const DELETE_PRODUCT = 'DELETE_PRODUCT';
 export const CREATE_PRODUCT = 'CREATE_PRODUCT';
 export const UPDATE_PRODUCT = 'UPDATE_PRODUCT';
 export const SET_PRODUCTS = 'SET_PRODUCTS';
 
 export const fetchProducts = () => {
-
     return async dispatch => {
         try {
             const response = await fetch('https://map-api-1531502761988.firebaseio.com/products.json');
             const resData = await response.json();
-
             const loadedProducts = [];
             for (const key in resData) {
                 const product = resData[key];
@@ -19,7 +16,6 @@ export const fetchProducts = () => {
             }
 
             dispatch({type: SET_PRODUCTS, products: loadedProducts});
-
         } catch (e) {
             throw  e;
         }
@@ -27,7 +23,12 @@ export const fetchProducts = () => {
 };
 
 export const deleteProduct = productId => {
-    return {type: DELETE_PRODUCT, productId: productId}
+    return async dispatch => {
+        await fetch(`https://map-api-1531502761988.firebaseio.com/products/${productId}.json`, {
+            method: 'DELETE',
+        });
+        dispatch({type: DELETE_PRODUCT, productId: productId});
+    };
 };
 
 export const createProduct = (title, description, price, imageUrl) => {
@@ -47,5 +48,14 @@ export const createProduct = (title, description, price, imageUrl) => {
 };
 
 export const updateProduct = (id, title, description, imageUrl) => {
-    return {type: UPDATE_PRODUCT, productId: id, productData: {title, description, imageUrl}}
+    return async dispatch => {
+        await fetch(`https://map-api-1531502761988.firebaseio.com/products/${id}.json`, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({title, description, imageUrl}),
+        });
+        dispatch({type: UPDATE_PRODUCT, productId: id, productData: {title, description, imageUrl}});
+    };
 };
