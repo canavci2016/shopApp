903512e5e0d28912c9e115c4317af8b63b21e030
Can <canavci2016@gmail.com>
2019-11-20 16:40:24 +0300

    Mapped Orders to User

--- store/actions/order.js
+++ store/actions/order.js
@@ -1,19 +1,20 @@
 import Order from "../../models/order";
+
 export const ADD_ORDER = 'ADD_ORDER';
 export const SET_ORDERS = 'SET_ORDERS';
 
 
 export const fetchOrders = () => {
-    return async dispatch => {
+    return async (dispatch, getStateFunc) => {
+        const userId = getStateFunc().auth.userId;
         try {
-            const response = await fetch('https://map-api-1531502761988.firebaseio.com/orders/u1.json');
+            const response = await fetch(`https://map-api-1531502761988.firebaseio.com/orders/${userId}.json`);
             const resData = await response.json();
             const loadedOrders = [];
             for (const key in resData) {
                 const order = resData[key];
                 loadedOrders.push(new Order(key, order.cartItems, order.totalAmount, new Date(order.date)));
             }
-
             dispatch({type: SET_ORDERS, orders: loadedOrders});
         } catch (e) {
             throw  e;
@@ -24,9 +25,10 @@ export const fetchOrders = () => {
 
 export const addOrder = (cartItems, totalAmount) => {
 
-    return async dispatch => {
+    return async (dispatch, getStateFunc) => {
+        const userId = getStateFunc().auth.userId;
         const date = new Date();
-        const response = await fetch('https://map-api-1531502761988.firebaseio.com/orders/u1.json', {
+        const response = await fetch(`https://map-api-1531502761988.firebaseio.com/orders/${userId}.json`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
@@ -36,11 +38,11 @@ export const addOrder = (cartItems, totalAmount) => {
 
         const resData = await response.json();
 
-        dispatch({
-            type: ADD_ORDER,
-            orderData: {id: resData.name, items: cartItems, amount: totalAmount, date}
-        });
-    };
+        if (!response.ok) {
+            throw  new Error(resData.error);
+        }
 
+        dispatch({type: ADD_ORDER, orderData: {id: resData.name, items: cartItems, amount: totalAmount, date}});
+    };
 };
 
